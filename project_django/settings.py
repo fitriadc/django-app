58c91f07b7226ed104c7152921be31a6cfc520a3
fitriadc <fitriadwicahya@gmail.com>
2022-09-01 12:09:53 +0700

    first commit

--- project_django/settings.py
+++ project_django/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
